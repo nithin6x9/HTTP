f71f9d2ea892410f639f097202dbbb188adb552a
nithin-nk <nithinkrishnalal303@gmail.com>
2023-12-11 22:23:07 +0530

    update

---
+++ C-httpserver/implement.c
@@ -0,0 +1,15 @@
+/*#include<arpa/inet.h>
+#include<errno.h>
+#include<stdio.h>
+#include<sys/socket.h>
+
+int main(){
+    int sockfd = socket(AF_INET,SOCK_STREAM,0);
+    if(sockfd == -1){
+        perror("webserver (socket)");
+        return 1;
+
+    }
+
+    print("Socket created successfully"); 
+}*/
---
+++ C-httpserver/webserver.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//#include<errno.h>
+//#include<sys/socket.h>
+
+int main(){
+  //  int sockfd = socket(AF_INET, SOCK_STREAM,0);
+    //if(sockfd == -1){
+      //  perror("webserver")
+    //}
+    printf("Hello world\n");
+    return 0;
+}
